bb2213def43c76b3e77fe630c1d6fcee774a69a4
monkho <ogh142003@gmail.com>
2022-07-11 22:40:21 -0500

    Added Persona and Tienda.java, as well a diagram

---
+++ src/Java/Tienda/Persona.java
@@ -0,0 +1,8 @@
+public class Persona {
+  protected String nombre;
+  protected String telefono;
+
+  protected Persona () {
+
+  }
+}
--- src/Java/Tienda/Tienda.java
+++ src/Java/Tienda/Tienda.java
@@ -1,10 +1,66 @@
 public class Tienda{
   private String nombre;
+  private String direccion;
+  private String rfc;
+  private String telefono;
+  private String horario;
 
+  // Constructores
   public Tienda(){
 
   }
-  public Tienda(String nombre){
+  public Tienda(String nombre, String direccion, String rfc, String telefono, String horario){
     this.nombre = nombre;
+    this.direccion = direccion;
+    this.rfc = rfc;
+    this.telefono = telefono;
+    this.horario = horario;
+  }
+
+  // Metodos
+  
+
+  // Getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getRfc() {
+		return rfc;
+	}
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+  public void mostrar(){
+    System.out.println("\t\tAbout Tienda:");
+    System.out.println("\nBienvenida/o a " + nombre);
+    System.out.println("Nos encuentras en: " + direccion);
+    System.out.println("Nuestro numero de contacto a tu disposicion: " + telefono);
+    System.out.println("Conoce nuestro horario de trabajo: " + horario);
   }
 }
